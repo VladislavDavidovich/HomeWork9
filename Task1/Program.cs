e100dbd60581c4e79742bfb7f9c90f75523b6072
Vladislav <Vlad.davidovich.2014@mail.ru>
2022-09-09 00:36:00 +0300

    Первая задача

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// // Показать натуральные числа от N до 1, N задано
+Console.WriteLine("Введите число ");
+int n = Convert.ToInt32 (Console.ReadLine());
+int m = 1;
+
+int NumberArray(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NumberArray(n, m + 1)}, ");
+    return m;
+}
+
+Console.WriteLine(NumberArray(n, m));
