7c1a837f2949195408ac2b3842771afe0ac26714
Vladislav <Vlad.davidovich.2014@mail.ru>
2022-09-09 00:37:27 +0300

    Четвертая задача

---
+++ Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Написать программу вычисления функции Аккермана
+
+Console.WriteLine("Введите первое число ");
+int n = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите первое число ");
+int m = Convert.ToInt32 (Console.ReadLine());
+
+int Akk(int n, int m)
+{ 
+    if (n == 0) 
+    return m + 1;
+
+    if (n != 0 && m == 0) 
+    return Akk(n - 1, 1);
+
+    if (n > 0 && m > 0) 
+    return Akk(n - 1, Akk(n, m - 1));
+    return Akk(n,m);
+}
+Console.WriteLine(Akk(n,m));
