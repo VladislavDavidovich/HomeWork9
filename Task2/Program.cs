2e64edd8608a0381d78ce6d85e7aab48d6896980
Vladislav <Vlad.davidovich.2014@mail.ru>
2022-09-09 00:36:23 +0300

    Вторая задача

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+Console.WriteLine("Введите первое число ");
+int n = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите первое число ");
+int m = Convert.ToInt32 (Console.ReadLine());
+
+
+int NumberArray(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NumberArray(n, m + 1)}, ");
+    return m;
+}
+
+// Console.WriteLine(NumberArray(n, m));
