aa45768721f54b1526aadfbe550c94e272ce96c3
Vladislav <Vlad.davidovich.2014@mail.ru>
2022-09-09 00:36:59 +0300

    Третья задача

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+Console.WriteLine("Введите первое число ");
+int n = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите первое число ");
+int m = Convert.ToInt32 (Console.ReadLine());
+
+
+int SummNumber(int n, int m)
+{
+    if (n == 0)
+        return 0;
+    else
+    return n + SummNumber(n - 1, m ) ;
+}
+
+Console.WriteLine($"Сумма элементов -> {SummNumber(n, m)}");
+
